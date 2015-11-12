f1bc30b902fa94e5f86a64042fb96f36f4faa349
mathiasberwig <d22m06123>
2015-11-12 11:00:07 -0200

    Updated TODO

--- app/src/main/java/com/acme/push/Config.java
+++ app/src/main/java/com/acme/push/Config.java
@@ -2,6 +2,7 @@ package com.acme.push;
 
 public class Config {
 
+    // TODO: Find a better way to store this settings
     public static class ADRESSES {
         public static final String SERVER_URL = "http://200.132.198.178/";
         public static final String INSERT_USER_URL = SERVER_URL + "Push-server/insert_user.php";
